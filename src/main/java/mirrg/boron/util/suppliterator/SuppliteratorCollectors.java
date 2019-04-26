482e21cdd843c5013b08ba8c1b6b84a222adc648
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-04-26 13:12:03 +0900

    fix

--- src/main/java/mirrg/boron/util/suppliterator/SuppliteratorCollectors.java
+++ src/main/java/mirrg/boron/util/suppliterator/SuppliteratorCollectors.java
@@ -6,6 +6,7 @@ import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import mirrg.boron.util.struct.ImmutableArray;
 import mirrg.boron.util.struct.Tuple;
